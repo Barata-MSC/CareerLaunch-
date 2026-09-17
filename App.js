80b45b1fd477169c63d23f098cceeccde316909d
Harry the NIGGA <seanharry0512@gmail.com>
2026-09-17 16:01:38 +0800

    Zoroimage

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Testing from './Testing';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Testing />
       <StatusBar style="auto" />
     </View>
   );
