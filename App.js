46993e7f3e7d52941f8af649d379e21f32dd223a
jape <jpv011720@gmail.com>
2026-09-18 23:25:57 +0800

    dashboard

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import WelcomeScreen from './WelcomeScreen/WelcomeScreen.js';
+import DashboardScreen from './DashboardScreen/DashboardScreen.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <WelcomeScreen />
+      <DashboardScreen />
       <StatusBar style="auto" />  
 
     </View>
